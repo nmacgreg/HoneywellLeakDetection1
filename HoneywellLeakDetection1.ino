a9341523e0d7452835f84ad5384c13e6c11b4513
Neil MacGregor <nmacgreg@ualberta.ca>
2024-11-05 13:09:43 -0700

    Modified with suggestions from ChatGPT for standardization

--- HoneywellLeakDetection1.ino
+++ HoneywellLeakDetection1.ino
@@ -1,12 +1,16 @@
 // Inspired by: https://forum.arduino.cc/t/interfacing-with-rope-leak-detector-cable/247230/13
 
+// Pin Assignments
 const int ledPin = LED_BUILTIN;
 const int leakPin = A0;
 const int buzzer = 9;
+// Some constants that might need adjusting, as I test the functionality
+const int DRY_THRESHOLD_LOW = 300;
+const int DRY_THRESHOLD_HIGH = 900;
+const int SAMPLE_COUNT = 20;
 
-int ledState = LOW;
-int DEBUG = 1;
-int timeout = 100;
+const bool DEBUG = 1;
+const int timeout = 100; // dwell time between sampling
 
 void setup() {
   // put your setup code here, to run once: 
@@ -21,39 +25,42 @@ void setup() {
 
 // When the cable is dry, over the course of about 3 seconds, the reading will cycle between 0 and 1023... sinusoidal?  (Capacitance?)
 // When the cable is wet, the reading will be in the range of approximately 500-800
-int dataSample (){
+bool isLeakDetected (){
   int sampleCounter = 0;
   int currentSample = 0;
-  // collect 20 samples
-  if (DEBUG) {
-    Serial.print("Datapoints: ");
-  }
-  for (sampleCounter = 0; sampleCounter<20; sampleCounter++) {
+  // collect samples
+  if (DEBUG) Serial.print("Datapoints: "); 
+  for (sampleCounter = 0; sampleCounter < SAMPLE_COUNT; sampleCounter++) {
     currentSample = analogRead(leakPin);
     if (DEBUG) {
       Serial.print(currentSample);
       Serial.print(", ");
     }
     delay(timeout);   
-    if (currentSample < 300 || currentSample > 900) {
+    if (currentSample < DRY_THRESHOLD_LOW || currentSample > DRY_THRESHOLD_HIGH) {
       return false;  // no leak
     }
   }
   return true;   // leak
 }
 
+void triggerAlarm() }
+  digitalWrite(ledPin, HIGH);
+  tone(buzzer, 1000);
+}
+
+void cancelAlarm() }
+  digitalWrite(ledPin, LOW);
+  noTone(buzzer);
+}
+
 void loop() {
   //Check for water leaks
-  if (dataSample()) {
-    ledState = HIGH;  // a leak has been detected
-    tone(buzzer, 1000);
+  if (isLeakDetected()) {
+    triggerAlarm();
   } else {
-    ledState = LOW;  // no leak 
-    noTone(buzzer);
+    cancelAlarm();
   }
-  digitalWrite(ledPin, ledState);
-  if (DEBUG) {
-    Serial.println("");
-  }
+  if (DEBUG) Serial.println("");
   delay (6000);
 }
